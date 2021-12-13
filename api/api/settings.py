a61799e7a6563808a4053525f793d4a21588ec2e
Vitor <93848966+VictorKudrin@users.noreply.github.com>
2021-12-13 23:21:09 +0900

    commit10

--- api/api/settings.py
+++ api/api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-inj&9bbk633+7^g%$bf04jd3h1a6jrquzjec)94=1np61&yb8+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "http://testingthetrade.com/",
